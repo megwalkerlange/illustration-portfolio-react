d0f0acdd702d463a75f771828c79ee0eb0ed4e49
Meg Walker <meg.walker56@gmail.com>
2023-10-09 21:26:50 +0100

    initial commit of files

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <>
+      <h1>Welcome!</h1>
+      <p>
+        My name is Meg and I am an Illustrator based in Birmingham, UK. You can
+        find out more about my work here and feel free to contact me if you have
+        a project in mind!
+      </p>
+    </>
+  );
+}
+
+export default App;

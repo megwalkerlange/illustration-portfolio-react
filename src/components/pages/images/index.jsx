8f34f81ee20e719b8f63e1fe98a6ee9f9bb33b82
Meg Walker <meg.walker56@gmail.com>
2023-10-28 22:57:48 +0100

    more images to slide

--- src/components/pages/images/index.jsx
+++ src/components/pages/images/index.jsx
@@ -4,6 +4,13 @@ import image1 from "../../assets/ant.jpg";
 import image2 from "../../assets/ferret .jpg";
 import image3 from "../../assets/dragon.jpg";
 import image4 from "../../assets/sleep.jpg";
+import image5 from "../../assets/troll 2.jpg";
+import image6 from "../../assets/repunzal.jpg";
+import image7 from "../../assets/sunbathing.jpg";
+import image8 from "../../assets/cake_Megan Walker copy.jpg";
+import image9 from "../../assets/sleepy boi.jpg";
+import image10 from "../../assets/dino.jpg";
+
 import "./../images/images.css";
 
 function Images() {
@@ -15,6 +22,12 @@ function Images() {
           <img src={image2} className="sliderimg" />
           <img src={image3} className="sliderimg" />
           <img src={image4} className="sliderimg" />
+          <img src={image5} className="sliderimg" />
+          <img src={image6} className="sliderimg" />
+          <img src={image7} className="sliderimg" />
+          <img src={image8} className="sliderimg" />
+          <img src={image9} className="sliderimg" />
+          <img src={image10} className="sliderimg" />
         </AliceCarousel>
       </div>
     </>
